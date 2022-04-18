a617b7699e25c9daee30c354a2d64f48b2c66cc1
maktub <98948028+Maktub0630@users.noreply.github.com>
2022-04-19 00:05:15 +0800

    Update flow.hpp

--- flow.hpp
+++ flow.hpp
@@ -3,12 +3,32 @@
 
 #ifndef flow_hpp
 #define flow_hpp
-
+#include <vector>
+struct record{
+    int money,debt,due;
+    bool hasDebt;
+    record(int money, int debt, int due, bool hasDebt){
+        this->debt=debt;
+        this->money=money;
+        this->due=due;
+        this->hasDebt=hasDebt;
+    }
+};
 
 void story();
 
 void menu();
 
-int play();
+void askLoan(std::vector <record> &records);
+
+void save(std::vector <record>& records, int money, int debt, int due, bool hasDebt);
+
+void play(std::vector <record>& records);
+
+void show(std::vector<record>& records);
+
+void loadornew(std::vector <record> &records);
+
+
 
 #endif
