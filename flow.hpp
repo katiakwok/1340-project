ea5d9434e1acf2470afde36cb927d1052927401b
maktub <98948028+Maktub0630@users.noreply.github.com>
2022-04-20 18:17:20 +0800

    Update flow.hpp

--- flow.hpp
+++ flow.hpp
@@ -4,6 +4,7 @@
 #ifndef flow_hpp
 #define flow_hpp
 #include <vector>
+//construct the data type for the record
 struct record{
     int money,debt,due;
     bool hasDebt;
@@ -14,19 +15,19 @@ struct record{
         this->hasDebt=hasDebt;
     }
 };
-
+//displaying the story
 void story();
-
+//displaying the menu
 void menu();
-
+//borrowing money
 void askLoan(std::vector <record> &records);
-
+//saving the game record to the save.txt
 void save(std::vector <record>& records, int money, int debt, int due, bool hasDebt);
-
+//function of a round of blackjack
 void play(std::vector <record>& records);
-
+//showing latest record
 void show(std::vector<record>& records);
-
+//pregame setting
 void loadornew(std::vector <record> &records);
 
 
