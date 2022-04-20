a297a10cbb385a9e0b019df0c26623fdffac0a29
maktub <98948028+Maktub0630@users.noreply.github.com>
2022-04-20 18:15:23 +0800

    Update card.hpp

--- card.hpp
+++ card.hpp
@@ -3,12 +3,16 @@
 #ifndef card_hpp
 #define card_hpp
 
+// distribution two cards to players and dealers to at the beginning
 void DealHand(int cards[]);
 
+//initiate players hand 
 int playerhand( int cards[]);
 
+//initiate dealers hand 
 int dealerhand( int cards[]);
 
+//hit action: drawing one card
 int hit(int hand[5]);
 
 #endif
